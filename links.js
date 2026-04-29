56b778fe3fceb9054edaccc9c0677ec232010bed
tobixhere-del <tobixhere@gmail.com>
2026-04-29 17:35:09 +0530

    Create links.js

---
+++ links.js
@@ -0,0 +1,9 @@
+// Add new vanities here: "path": "roblox_url"
+const vanityLinks = {
+  "/test1": "https://roblox.com",
+  "/test2": "https://roblox.com",
+  "/cool-guy": "https://roblox.com"
+};
+
+// Export it so index.html can see it
+window.vanityLinks = vanityLinks;
